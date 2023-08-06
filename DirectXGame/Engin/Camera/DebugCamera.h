5c98372dcdb4da1208e97f6188bdbc01d976dc00
yamashitatubasa <k021g1247@g.neec.ac.jp>
2023-08-06 23:54:27 +0900

    コライダーの追加

--- DirectXGame/Engin/Camera/DebugCamera.h
+++ DirectXGame/Engin/Camera/DebugCamera.h
@@ -1,5 +1,17 @@
 #pragma once
+
+#include <DirectXMath.h>
+
+/// <summary>
+/// デバックカメラ
+/// </summary>
 class DebugCamera
 {
+	// X,Y,Z軸のローカル回転角
+	DirectX::XMFLOAT3 rotation_ = { 0,0,0 };
+
+	// ローカル座標
+	DirectX::XMFLOAT3 translation_ = { 0,0,-50 };
 };
+
 
