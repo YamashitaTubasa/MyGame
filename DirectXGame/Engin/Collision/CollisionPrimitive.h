c902ace51dc9ecdbf93dfa786fafb29fbc1e909c
yamashitatubasa <k021g1247@g.neec.ac.jp>
2023-08-14 23:23:42 +0900

    レイの当たり判定追加

--- DirectXGame/Engin/Collision/CollisionPrimitive.h
+++ DirectXGame/Engin/Collision/CollisionPrimitive.h
@@ -8,7 +8,8 @@
 /// <summary>
 /// 球
 /// </summary>
-struct Sphere {
+struct Sphere 
+{
 	// 抽選座標
 	DirectX::XMVECTOR center = { 0,0,0,1 };
 	// 半径
@@ -18,9 +19,41 @@ struct Sphere {
 /// <summary>
 /// 平面
 /// </summary>
-struct Plane {
+struct Plane 
+{
 	// 法線ベクトル
 	DirectX::XMVECTOR normal = { 0,1,0,0 };
 	// 原点(0,0,0)からの距離
 	float distance = 0.0f;
+};
+
+/// <summary>
+/// レイ（半直線）
+/// </summary>
+struct Ray
+{
+	// 始点座標
+	DirectX::XMVECTOR start = { 0,0,0,1 };
+	// 方向
+	DirectX::XMVECTOR dir = { 1,0,0,0 };
+};
+
+/// <summary>
+/// 法線付き三角形（時計回りが表面）
+/// </summary>
+class Triangle 
+{
+public:
+	// 頂点座標3つ
+	DirectX::XMVECTOR p0;
+	DirectX::XMVECTOR p1;
+	DirectX::XMVECTOR p2;
+	// 法線ベクトル
+	DirectX::XMVECTOR normal;
+
+public:
+	/// <summary>
+	/// 法線の計算
+	/// </summary>
+	void ComputeNormal();
 };
