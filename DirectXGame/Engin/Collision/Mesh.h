c0895f0606fb57c98708b813710164316f025837
yamashitatubasa <k021g1247@g.neec.ac.jp>
2023-08-25 23:38:21 +0900

    メッシュの処理追加

--- DirectXGame/Engin/Collision/Mesh.h
+++ DirectXGame/Engin/Collision/Mesh.h
@@ -1,5 +1,20 @@
 #pragma once
+
+#include <vector>
+
 class Mesh
 {
+public:
+	/// <summary>
+	/// 頂点配列を取得
+	/// </summary>
+	/// <returns>頂点配列</returns>
+	inline const std::vector<VectorPosNormalUv>& GetVertices() { return vertices; }
+
+	/// <summary>
+	/// インデックス配列を取得
+	/// </summary>
+	/// <returns>インデックス配列</returns>
+	inline const std::vector<unsigned short>& GetIndices() { return indices; }
 };
 
