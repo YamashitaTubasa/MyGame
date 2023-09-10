efacbfaf1684bc5f9d58779e0cd5cb4e0fbdee86
yamashitatubasa <k021g1247@g.neec.ac.jp>
2023-09-10 22:33:44 +0900

    シーンマ管理の処理の追加

--- DirectXGame/Engin/Scene/AbstractSceneFactory.h
+++ DirectXGame/Engin/Scene/AbstractSceneFactory.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include "GameBaseScene.h"
+#include <string>
+
+/// <summary>
+/// シーン工場
+/// </summary>
+class AbstractSceneFactory
+{
+public:
+	/// 仮想デストラクタ
+	virtual ~AbstractSceneFactory() = default;
+
+	/// <summary>
+	/// シーン生成
+	/// </summary>
+	/// <param name="sceneName">シーン名</param>
+	/// <returns></returns>
+	virtual GameBaseScene* CreateScene(const std::string& sceneName) = 0;
+};
