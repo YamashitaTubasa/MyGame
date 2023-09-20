c6f83b9fff41095759a18acb9cc7416d8fb7a07c
yamashitatubasa <k021g1247@g.neec.ac.jp>
2023-09-20 23:51:10 +0900

    タイトルの演出

--- DirectXGame/Application/Scene/GameTitleScene.cpp
+++ DirectXGame/Application/Scene/GameTitleScene.cpp
@@ -8,67 +8,118 @@ GameTitleScene::GameTitleScene()
 
 GameTitleScene::~GameTitleScene()
 {
-	delete particleMan1;
+	delete sprite_;
+	delete title_;
 }
 
 void GameTitleScene::Initialize()
 {
-	//particleMan1 = ParticleManager::Create();
-	//particleMan1->LoadTexture("Resources/effect1.png");
-	//particleMan1->Update();
+	// DirectXCommonのインスタンス取得
+	dxCommon_ = DirectXCommon::GetInstance();
 
-	//for (int i = 0; i < 100; i++) {
-	//	// X,Y,Zすべて[-5.0f,+5.0f]でランダムに分布
-	//	const float md_pos = 10.0f;
-	//	XMFLOAT3 pos{};
-	//	float px = 5;
-	//	float py = 0;
-	//	float pz = 0;
-	//	pos.x = (float)rand() / RAND_MAX * px - px / 2.0f + px;
-	//	pos.y = (float)rand() / RAND_MAX * py - py / 2.0f + py;
-	//	pos.z = (float)rand() / RAND_MAX * pz - pz / 2.0f;
-	//	// X,Y,Z全て[-0.05f,+0.05f]でランダム分布
-	//	const float md_vel = 0.1f;
-	//	XMFLOAT3 vel{};
-	//	vel.x = (float)rand() / RAND_MAX * md_vel - md_vel / 2.0f;
-	//	vel.y = (float)rand() / RAND_MAX * md_vel - md_vel / 2.0f;
-	//	vel.z = (float)rand() / RAND_MAX * md_vel - md_vel / 2.0f;
-	//	// 重力に見立ててYのみ[-0.001f,0]でランダム分布
-	//	XMFLOAT3 acc{};
-	//	const float md_acc = 0.001f;
-	//	acc.y = (float)rand() / RAND_MAX * md_acc;
+	// 天球の初期化
+	skydome = new Skydome();
+	skydome->Initialize();
 
-	//	// 追加
-	//	particleMan1->Add(60, pos, vel, acc, 1.0f, 0.0);
-	//}
+	// スプライトの初期化
+	sprite_ = new Sprite();
+	spriteCommon_ = sprite_->SpriteCommonCreate(1280, 720);
+
+	//===== タイトル描画の初期化 =====//
+	title_ = new Sprite();
+	// テクスチャの読み込み
+	title_->LoadTexture(spriteCommon_, 1, L"Resources/Image/title2.png");
+	// スプライトの生成
+	title_->SpriteCreate(1280, 720, 1, spriteCommon_, XMFLOAT2(0.0f, 0.0f), false, false);
+	// 色、座標、サイズ、回転角の設定
+	title_->SetColor(titleColor_);
+	title_->SetPosition(titlePos_);
+	title_->SetScale(titleScale_);
+	title_->SetRotation(titleRot_);
+	// スプライトの頂点バッファの転送
+	title_->SpriteTransferVertexBuffer(title_, spriteCommon_, 1);
+
+	// OBJモデルの読み込み
+	model = Model::LoadFromOBJ("fighter");
+	// オブジェクトの生成
+	object3d = Object3d::Create();
+	// オブジェクトにモデルをセット
+	object3d->SetModel(model);
+	// オブジェクトの位置の設定
+	object3d->SetPosition(pPos);
+	object3d->SetScale({5,5,5});
+	object3d->SetRotation({0,0,0});
+	object3d->SetTarget(object3d->GetPosition());
+	object3d->SetEye(eye);
 }
 
 void GameTitleScene::Update()
 {
-	//// カメラ移動
-	//if (input->PushKey(DIK_W) || input->PushKey(DIK_S) || input->PushKey(DIK_D) || input->PushKey(DIK_A))
-	//{
-	//	if (input->PushKey(DIK_W)) { ParticleManager::CameraMoveEyeVector({ 0.0f,+1.0f,0.0f }); }
-	//	else if (input->PushKey(DIK_S)) { ParticleManager::CameraMoveEyeVector({ 0.0f,-1.0f,0.0f }); }
-	//	if (input->PushKey(DIK_D)) { ParticleManager::CameraMoveEyeVector({ +1.0f,0.0f,0.0f }); }
-	//	else if (input->PushKey(DIK_A)) { ParticleManager::CameraMoveEyeVector({ -1.0f,0.0f,0.0f }); }
-	//}
+	// 天球の更新
+	skydome->Update();
 
-	//particleMan1->Update();
+	// 自機の更新
+	object3d->Update();
+
+	// スプライトの更新
+	title_->SpriteUpdate(title_, spriteCommon_);
+
+	// シーンの切り替え
+	if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
+		start = true;
+	}
+	if (start) {
+		startTimer++;
+		pPos.z += 1.5f;
+		pPos.y += 0.25f;
+		eye.y -= 0.06f;
+
+		object3d->SetPosition(pPos);
+		object3d->SetEye(eye);
+	}
+	if (startTimer >= 200) {
+		start = false;
+		startTimer = 0;
+		// ゲームプレイシーン（次シーン）を生成
+		GameSceneManager::GetInstance()->ChangeScene("GAMEPLAY");
+	}
 }
 
-void GameTitleScene::Draw(DirectXCommon* dXCommon)
+void GameTitleScene::Draw()
 {
-	// 3Dオブジェクト描画前処理
-	ParticleManager::PreDraw(dXCommon->GetCommandList());
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* cmdList = dxCommon_->GetCommandList();
 
-	// 3Dオブクジェクトの描画
-	//particleMan1->Draw();
+#pragma region オブジェクトの描画
 
-	/// <summary>
-	/// ここに3Dオブジェクトの描画処理を追加できる
-	/// </summary>
+	// オブジェクトの前処理
+	Object3d::PreDraw(cmdList);
 
-	// 3Dオブジェクト描画後処理
-	ParticleManager::PostDraw();
+	//=== オブジェクトの描画 ===//
+	skydome->Draw();
+	object3d->Draw();
+
+	// オブジェクトの後処理
+	Object3d::PostDraw();
+
+#pragma endregion
+
+#pragma region スプライトの描画
+
+	// スプライト描画前処理
+	Sprite::PreDraw(cmdList, spriteCommon_);
+
+	//=== スプライトの描画 ===//
+	if (!start) {
+		title_->SpriteDraw(spriteCommon_);
+	}
+	
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
+
+void GameTitleScene::Finalize()
+{
 }
--- DirectXGame/Application/Scene/GameTitleScene.h
+++ DirectXGame/Application/Scene/GameTitleScene.h
@@ -1,24 +1,71 @@
 #pragma once
-#include "ParticleManager.h"
-#include "Input.h"
+
 #include "DirectXCommon.h"
+#include "Input.h"
+#include "Sprite.h"
+#include "Object3d.h"
+#include "Model.h"
+#include "Skydome.h"
+#include "GameBaseScene.h"
+#include "GameSceneManager.h"
+#include "GamePlayScene.h"
+
 #include <DirectXMath.h>
 
-class GameTitleScene 
+class GameTitleScene : public GameBaseScene
 {
-public:
+public: // メンバ関数
+	// コンストラクタ
 	GameTitleScene();
+
+	// デストラクタ
 	~GameTitleScene();
 
-public:
-	void Initialize();
+public: // メンバ関数
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize() override;
 
-	void Update();
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update() override;
 
-	void Draw(DirectXCommon* dXCommon);
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// <param name="dXCommon"></param>
+	void Draw() override;
 
-private:
-	ParticleManager* particleMan1 = nullptr;
-	Input* input = nullptr;
-	DirectXCommon* dXCommon = nullptr;
+	/// <summary>
+	/// 解放
+	/// </summary>
+	void Finalize() override;
+
+private: // メンバ変数
+	Input* input_ = nullptr;
+	DirectXCommon* dxCommon_ = nullptr;
+	
+	// スプライト
+	Sprite* sprite_ = nullptr;
+	Sprite* title_ = nullptr;
+
+	// 天球
+	Skydome* skydome = nullptr;
+
+	// オブジェクト
+	Object3d* object3d = nullptr;
+	Model* model = nullptr;
+
+	DirectX::XMFLOAT4 titleColor_ = { 1,1,1,1 };
+	DirectX::XMFLOAT3 titlePos_ = { 250,200,0 };
+	DirectX::XMFLOAT2 titleScale_ = { 780,160 };
+
+	DirectX::XMFLOAT3 pPos = { 0,-5,-30 };
+	DirectX::XMFLOAT3 eye = { 0,0,-50 };
+
+	float titleRot_ = 0.0f;
+	bool start = false;
+	int startTimer = 0;
 };
